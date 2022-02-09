7c9c729de5446ece41249f91109f3e8ac1ee0179
Ilia Marchukov <Gerald16311@gmail.com>
2022-02-09 13:59:19 +0400

    DZ1_Done

--- l1_t1.py
+++ l1_t1.py
@@ -1,6 +1,5 @@
 import requests
 import json
-from pprint import pprint
 
 user_name = "Gerald16311"
 url = f"https://api.github.com/users/{user_name}/repos"
@@ -8,7 +7,7 @@ url = f"https://api.github.com/users/{user_name}/repos"
 response = requests.get(url)
 j_data = response.json()
 
-with open('parsed_data.json', 'w') as outfile:
+with open(f'parsed_data_{user_name}.json', 'w') as outfile:
     json.dump(j_data, outfile)
 
 print(f'У пользователя {user_name} есть вот такой список репозиториев:')
